a6cb3d5aa60af9dc120eb6b6f3cb8a348be2c387
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-25 18:41:22 -0400

    project done

---
+++ script.py
@@ -0,0 +1,33 @@
+input_string = '&Hero21438uu321jdsdaw#&*#'
+
+dict = {
+  '{' : "&#123;",
+  '}' : "&#125;",
+  '@' : "&#64;",
+  '&' : "&#amp;",
+  '(' : "&#40;",
+  '#' : "&#35;",
+  ')' : "&#41;",
+  '[' : "&#91;",
+  ']' : "&#93;",
+  "^" : "&#94;",
+  "*" : "&ast;",
+  "\"" : "&quot;",
+  '%' : "&#37;",
+  '$' : "&#36;"
+  # add any more
+}
+
+answer = ""
+
+
+for i in input_string:
+  if i in dict:
+    answer+= dict[i]
+  else:
+    answer+=i
+
+
+f = open("file.txt", "w")
+
+f.write(answer)
